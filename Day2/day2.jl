a0f216d8944794f1c5fd59e16883a3444ccbbef8
d1ff1cult0 <jarne.plessers@student.kuleuven.be>
2024-12-02 18:37:02 +0100

    Small change

--- Day2/day2.jl
+++ Day2/day2.jl
@@ -8,10 +8,8 @@ catch e
 end
 
 function part1()
-    strictly_increasing_or_decreasing = [report for report in lines_int 
-                                        if all(report[i] > report[i-1] for i in 2:length(report)) || 
-                                           all(report[i] < report[i-1] for i in 2:length(report))]
-    no_jumps_bigger_than_3 = [report for report in strictly_increasing_or_decreasing
+    strict_inc_or_dec = [report for report in lines_int if issorted(report) || issorted(report, rev=true)]
+    no_jumps_bigger_than_3 = [report for report in strict_inc_or_dec
                               if all(abs(report[i]-report[i-1]) <= 3 for i in 2:length(report))]
     return length(no_jumps_bigger_than_3)
 end
@@ -19,9 +17,9 @@ end
 println(part1())
 
 function part2()
-    strictly_increasing_or_decreasing = [(report, min(sum([1 for i in 2:length(report) if !all(report[i] < report[j] for j in 1:i-1)]), 
-                                                      sum([1 for i in 2:length(report) if !all(report[i] > report[j] for j in 1:i-1)]))) 
-                                        for report in lines_int]
+    strict_inc_or_dec = [(report, min(sum([1 for i in 2:length(report) if !all(report[i] < report[j] for j in 1:i-1)]), 
+                                      sum([1 for i in 2:length(report) if !all(report[i] > report[j] for j in 1:i-1)]))) 
+                        for report in lines_int]
     no_jumps_bigger_than_3 = [(report, faults+sum([1 for i in 2:length(report) if abs(report[i]-report[i-1]) > 3])) 
                              for (report, faults) in strictly_increasing_or_decreasing]
     return sum([1 for (report, faults) in no_jumps_bigger_than_3 if faults <= 1])
